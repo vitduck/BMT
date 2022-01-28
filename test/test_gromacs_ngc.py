bdeca189ab78a739c615e195bf9ca3de858e523e
vitduck <levietduc@kaist.ac.kr>
2022-01-28 17:44:03 +0900

    Reduce GROMACS NSTEP

--- test/test_gromacs_ngc.py
+++ test/test_gromacs_ngc.py
@@ -6,7 +6,7 @@ gmx = Gromacs(
     prefix = '../run/GROMACS', 
     input  = '../input/GROMACS/water_1536.tpr',
     sif    = '../image/gromacs-2021.3.sif',
-    nsteps = 60000 )  
+    nsteps = 20000 )  
 
 gmx.info()
 gmx.run()
