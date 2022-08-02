1b2b7444f23d3009ac03014f9955b86f9c1ac845
vitduck <levietduc@kaist.ac.kr>
2022-08-02 14:04:29 +0900

    Fix SLURM_NODE_LIST parser

--- src/slurm.py
+++ src/slurm.py
@@ -3,17 +3,25 @@
 import os
 import re
 
-def slurm_nodelist(): 
-    nodelist    = [] 
-    name, index = re.search('([a-zA-Z]+)\[?([0-9\-,]+)\]?', os.environ['SLURM_NODELIST']).group(1,2)
+def slurm_nodelist():
+    nodelist    = []
 
-    for node in index.split(','):
-        if re.search('-', node):
-            istart, iend = node.split('-')
-                
-            # return the list of node with leading zeroes
-            nodelist += [ name + str(node).zfill(len(istart)) for node in range(int(istart), int(iend)+1) ]
-        else:
-            nodelist += [ name + str(node) ]
+    match = re.search('(.*?)\[(.*?)\]', os.environ['SLURM_NODELIST'])
+
+    # multi-node job
+    if match:
+        name, index = match.groups()
+
+        for node in index.split(','):
+            if re.search('-', node):
+                istart, iend = node.split('-')
+
+                # return the list of node with leading zeroes
+                nodelist += [name + str(node).zfill(len(istart)) for node in range(int(istart), int(iend)+1)]
+            else:
+                nodelist += [ name + str(node) ]
+    # single-node job / local host
+    else:
+        nodelist = [os.environ['SLURM_NODELIST']]
 
     return nodelist
